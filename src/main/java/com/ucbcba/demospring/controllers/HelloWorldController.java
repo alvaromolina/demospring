8fadc2b59a56519f224bf0e53556dc1f7f409b68
alvaromolinac@gmail.com <alvaromolinac@gmail.com>
2018-09-18 08:21:33 -0400

    controller thymleaf

--- src/main/java/com/ucbcba/demospring/controllers/HelloWorldController.java
+++ src/main/java/com/ucbcba/demospring/controllers/HelloWorldController.java
@@ -1,16 +1,21 @@
 package com.ucbcba.demospring.controllers;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String index() {
-        return "Hello World";
+    @RequestMapping(value = "/{mensaje}", method = RequestMethod.GET)
+    public String index(@PathVariable String mensaje, Model model) {
+        mensaje = mensaje.substring(0,4);
+        model.addAttribute("message", mensaje);
+        return "hello";
     }
 
 
